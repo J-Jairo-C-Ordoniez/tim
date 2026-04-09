aa6b295f8e7504888127dfebad4a8fd64dc115a8
J-Jairo-C-Ordoniez <cordobaojhonjairo21@gmail.com>
2026-04-08 21:42:12 -0500

    TIM: update mee

--- src/components/home/main/Main.js
+++ src/components/home/main/Main.js
@@ -19,7 +19,7 @@ export default function Main() {
     const [isUploadModalOpen, setIsUploadModalOpen] = useState(false)
     const [isLegacyActive, setIsLegacyActive] = useState(false)
     const [withAudio, setWithAudio] = useState(true)
-    const progression = contributions.length
+    const progression = 120 // Forzado a 120 para pruebas de producción
     const totalSlots = 120
 
     useEffect(() => {
